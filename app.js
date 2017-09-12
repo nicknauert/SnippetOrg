0342f8fddd776e4b91d1a1125ab3c0ef6d2fb6da
Nick Nauert <nicknauert@gmail.com>
2017-09-12 11:53:52 -0400

    getting hosting to work

--- app.js
+++ app.js
@@ -22,7 +22,14 @@ const { createToken, ensureAuthentication } = require('./auth/helpers.js')
 require('dotenv').config()
 
 
-mongoose.connect('mongodb://localhost:27017/snippetdb');
+// mongoose.connect('mongodb://localhost:27017/snippetdb');
+
+var MongoClient = require('mongodb').MongoClient;
+
+var uri = "mongodb://kay:" + process.env.PASSWORD + "@mycluster0-shard-00-00-wpeiv.mongodb.net:27017,mycluster0-shard-00-01-wpeiv.mongodb.net:27017,mycluster0-shard-00-02-wpeiv.mongodb.net:27017/admin?ssl=true&replicaSet=Mycluster0-shard-0&authSource=admin";
+MongoClient.connect(uri, function(err, db) {
+  db.close();
+});
 
 app.engine('mustache', mustache())
 app.set('view engine', 'mustache')
