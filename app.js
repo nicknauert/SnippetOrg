0e1b917c1569594215c158262f853656ebaa6c4e
Nick Nauert <nicknauert@gmail.com>
2017-09-12 11:58:27 -0400

    getting hosting to work

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ require('dotenv').config()
 
 var MongoClient = require('mongodb').MongoClient;
 
-var uri = "mongodb://kay:" + process.env.PASSWORD + "@mycluster0-shard-00-00-wpeiv.mongodb.net:27017,mycluster0-shard-00-01-wpeiv.mongodb.net:27017,mycluster0-shard-00-02-wpeiv.mongodb.net:27017/admin?ssl=true&replicaSet=Mycluster0-shard-0&authSource=admin";
+var uri = "mongodb://kay:CRRTLKCpwpKW4l2s@mycluster0-shard-00-00-wpeiv.mongodb.net:27017,mycluster0-shard-00-01-wpeiv.mongodb.net:27017,mycluster0-shard-00-02-wpeiv.mongodb.net:27017/admin?ssl=true&replicaSet=Mycluster0-shard-0&authSource=admin";
 MongoClient.connect(uri, function(err, db) {
   db.close();
 });
